101e3830cb4ac3ea5627dfe0d81878bc9c4252ec
aoikurokawa <ht.so.ik0102@gmail.com>
2024-01-09 16:56:09 +0900

    feat: Reading in Lines of Arbitrary

--- hello.c
+++ hello.c
@@ -1,16 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+char *readline(FILE *fp) {
+  int offset = 0;
+  int bufsize = 4;
+  char *buf;
+  int c;
+
+  buf = malloc(bufsize);
+
+  if (buf == NULL) {
+    return NULL;
+  }
+
+  while (c = fgetc(fp), c != '\n' && c != EOF) {
+    if (offset == bufsize - 1) {
+      bufsize *= 2;
+
+      char *new_buf = realloc(buf, bufsize);
+
+      if (new_buf == NULL) {
+        free(buf);
+        return NULL;
+      }
+
+      buf = new_buf;
+    }
+
+    buf[offset++] = c;
+  }
+
+  if (c == EOF && offset == 0) {
+    free(buf);
+    return NULL;
+  }
+
+  if (offset < bufsize - 1) {
+    char *new_buf = realloc(buf, offset + 1);
+
+    if (new_buf != NULL) {
+      buf = new_buf;
+    }
+  }
+
+  buf[offset] = '\0';
+
+  return buf;
+}
 
 int main(void) {
-  int *p = malloc(sizeof(int) * 10);
+  FILE *fp = fopen("whale.txt", "r");
 
-  for (int i = 0; i < 10; i++) {
-    p[i] = i * 5;
+  char *line;
+
+  while ((line = readline(fp)) != NULL) {
+    printf("%s\n", line);
+    free(line);
   }
 
-  for (int i = 0; i < 10; i++) {
-    printf("%d\n", p[i]);
-  }
-
-  free(p);
+  fclose(fp);
 }
