bcad78c723a6422bc54a1b8d6a9ff8bc739f7339
aoikurokawa <ht.so.ik0102@gmail.com>
2024-01-09 15:10:22 +0900

    feat: void Pointers

--- hello.c
+++ hello.c
@@ -10,4 +10,21 @@ int my_strlen(char *s) {
   return p - s;
 }
 
-int main(void) { printf("%d\n", my_strlen("Hello, world!")); }
+int main(void) {
+  int a[] = {11, 22, 33, 44, 55};
+  int *p = a;
+
+  for (int i = 0; i < 5; i++) {
+    printf("%d\n", a[i]);
+  }
+  for (int i = 0; i < 5; i++) {
+    printf("%d\n", p[i]);
+  }
+  for (int i = 0; i < 5; i++) {
+    printf("%d\n", *a + 1);
+  }
+  for (int i = 0; i < 5; i++) {
+    printf("%d\n", *p + 1);
+  }
+  // printf("%d\n", my_strlen("Hello, world!"));
+}
