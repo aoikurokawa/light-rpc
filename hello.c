d3221537e935965deab4564468c37cc6d148952f
aoikurokawa <ht.so.ik0102@gmail.com>
2024-01-09 16:03:55 +0900

    feat: allocating and deallocating

--- hello.c
+++ hello.c
@@ -1,48 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-struct animal {
-  char *name;
-  int leg_count;
-};
-
-int compar(const void *elem1, const void *elem2) {
-  const struct animal *animal1 = elem1;
-  const struct animal *animal2 = elem2;
-
-  if (animal1->leg_count > animal2->leg_count) {
-    return 1;
-  }
-
-  if (animal1->leg_count < animal2->leg_count) {
-    return -1;
-  }
-
-  return 0;
-}
 
 int main(void) {
-  struct animal a[4] = {
-      {.name = "Dog", .leg_count = 4},
-      {.name = "Monkey", .leg_count = 2},
-      {.name = "Antelope", .leg_count = 4},
-      {.name = "Snake", .leg_count = 0},
-  };
+  int *p = malloc(sizeof(int));
 
-  qsort(a, 4, sizeof(struct animal), compar);
+  *p = 12;
 
-  for (int i = 0; i < 4; i++) {
-    printf("%d: %s\n", a[i].leg_count, a[i].name);
-  }
-}
+  printf("%d\n", *p);
 
-void *my_memcpy(void *dest, void *src, int byte_count) {
-  char *s = src, *d = dest;
+  free(p);
 
-  while (byte_count--) {
-    *d++ = *s++;
-  }
-
-  return dest;
+  printf("%d\n", *p);
 }
