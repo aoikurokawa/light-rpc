3f87a138c47f0c376c5045001fe40378ec4f98b1
aoikurokawa <ht.so.ik0102@gmail.com>
2024-01-09 12:33:31 +0900

    feat: read text from file

--- hello.c
+++ hello.c
@@ -2,13 +2,14 @@
 
 int main(void) {
   FILE *fp;
-  char s[1024];
-  int linecount = 0;
+  char name[1024];
+	float length;
+	int mass;
 
-  fp = fopen("quote.txt", "r");
+  fp = fopen("whale.txt", "r");
 
-  while (fgets(s, sizeof(s), fp) != NULL) {
-    printf("%d: %s", ++linecount, s);
+  while (fscanf(fp, "%s %f %d", name, &length, &mass) != EOF) {
+    printf("%s whale, %d tonnes, %.1f meters\n", name, mass, length);
   }
 
   fclose(fp);
