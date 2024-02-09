a48770071bbb4861ba74cd9531968a71e40fe9e4
aoikurokawa <ht.so.ik0102@gmail.com>
2024-02-10 08:58:35 +0900

    feat: add tracing

--- intro/src/main.rs
+++ intro/src/main.rs
@@ -1,8 +1,30 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
+use tracing::{debug, error, info, span, trace, warn, Level};
 
 use chrono::Utc;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    for _file in std::env::args() {
+        let span = span!(Level::INFO, "file");
+        let _guard = span.enter();
+        info!("opening the file");
+        // let mut file = std::fs::File::open(file).unwrap();
+        info!("reading file contents");
+        // let mut bytes = Vec::new();
+        // file.read(&mut bytes).unwrap();
+        info!("parsing");
+
+        info!("done with that")
+    }
+}
+
+pub fn tcp() {
     let listner = TcpListener::bind("127.0.0.1:8080").unwrap();
     loop {
         match listner.accept() {
